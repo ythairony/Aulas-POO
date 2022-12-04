82ad0d18d1f784ac37d4ce4846afbc1902ad8b8a
yurithairony <102562875+yurithairony@users.noreply.github.com>
2022-12-04 03:02:54 +0000

    questão 1180

---
+++ Listas/POO06/1180.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program {
+  public static void Main() {
+    int n = int.Parse(Console.ReadLine());
+    int[] x = new int[n];
+    for (int i = 0; i < n; i++) {
+      x[i] = int.Parse(Console.ReadLine());  
+    }
+    int menor = x[0];
+    int index = 0;
+    for (int j = 1; j < n; j++) {
+      if (x[j] < menor) {
+        menor = x[j];
+        index = j;
+      }
+    }
+    Console.WriteLine($"Menor valor: {menor}");
+    Console.WriteLine($"Posicao: {index}");
+  }
+}
